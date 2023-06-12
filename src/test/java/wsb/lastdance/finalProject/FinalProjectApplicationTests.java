08362bea22c057b4db33e6cc5a0fc45c1a1b70de
beardandbeer <marcin.krupa1999@gmail.com>
2023-06-12 13:42:01 +0200

    Nowy Projekt

---
+++ src/test/java/wsb/lastdance/finalProject/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.lastdance.finalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
